6d583fe50696c4a4651ff6c9e5aff8f927273ea2
Steven Mackey <stevester94@gmail.com>
2016-05-31 15:48:09 -0700

    working

--- app/src/main/java/squirrelvalleysoftworks/com/snaptimer/MainActivity.java
+++ app/src/main/java/squirrelvalleysoftworks/com/snaptimer/MainActivity.java
@@ -2,15 +2,21 @@ package squirrelvalleysoftworks.com.snaptimer;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button startButton;
+    private EditText delayTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +33,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {startButtonHandler();}
         });
 
+        delayTextField = (EditText) findViewById(R.id.delayTextField);
+
     }
 
     private void startButtonHandler() {
-        System.out.println("Clicked");
+        System.out.println("Clicked!");
+        Intent intent = new Intent(this, TriggerService.class);
+        PendingIntent pending = PendingIntent.getService(this, 0, intent, 0);
 
-        Intent intent = new Intent(MainActivity.this, TriggerService.class);
-        PendingIntent pending = PendingIntent.getService(MainActivity.this, 0, intent, 0);
+        long timeOut = Long.parseLong(delayTextField.getText().toString());
+        timeOut = timeOut * 1000; // Seconds to milliseconds
+        timeOut = timeOut + System.currentTimeMillis(); // Actual time to be triggered
 
-        AlarmManager manager = (AlarmManager)
+        AlarmManager manager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        manager.set(AlarmManager.RTC, timeOut, pending);
 
     }
 }
--- app/src/main/java/squirrelvalleysoftworks/com/snaptimer/TriggerService.java
+++ app/src/main/java/squirrelvalleysoftworks/com/snaptimer/TriggerService.java
@@ -6,10 +6,19 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
 /**
  * Created by smackey2 on 5/31/2016.
  */
 public class TriggerService extends Service {
+    TriggerService() {
+        super();
+        System.out.println("Constructed");
+    }
     @Override
     public void onCreate() {
         Log.d("Trigger", "Trigger service created");
@@ -18,6 +27,16 @@ public class TriggerService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("Trigger", "Trigger service started");
+        try {
+            Process p = Runtime.getRuntime().exec("su");
+            OutputStream stdIn = p.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdIn));
+
+            writer.write("input keyevent 24\n");
+            writer.flush();
+        } catch (Exception e) {
+            System.err.println("Error triggering");
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 
